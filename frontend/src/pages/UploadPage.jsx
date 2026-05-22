ab135734bea0fc61f1b20a7311f5aff4fe5cd07e
KANAND6031 <krishnaanand2984@gmail.com>
2026-05-22 21:26:23 +0000

    frontend changes

---
+++ frontend/src/pages/UploadPage.jsx
@@ -0,0 +1,11 @@
+const UploadPage = () => {
+    return (
+        <div className="p-10">
+            <h1 className="text-3xl font-bold">
+                Upload SOP PDFs
+            </h1>
+        </div>
+    );
+};
+
+export default UploadPage;
