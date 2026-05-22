cd7fe8c2466d4a18d266c404c8d1fc01598f16b0
KANAND6031 <krishnaanand2984@gmail.com>
2026-05-22 21:31:06 +0000

    ui change

--- frontend/src/pages/UploadPage.jsx
+++ frontend/src/pages/UploadPage.jsx
@@ -1,9 +1,29 @@
 const UploadPage = () => {
     return (
-        <div className="p-10">
-            <h1 className="text-3xl font-bold">
-                Upload SOP PDFs
-            </h1>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="bg-white p-10 rounded-2xl shadow-lg w-[500px]">
+                
+                <h1 className="text-4xl font-bold text-center mb-6">
+                    OpsMind AI
+                </h1>
+
+                <p className="text-gray-600 text-center mb-8">
+                    Upload SOP PDF documents
+                </p>
+
+                <input
+                    type="file"
+                    accept=".pdf"
+                    className="w-full border p-3 rounded-lg"
+                />
+
+                <button
+                    className="w-full mt-5 bg-black text-white py-3 rounded-lg hover:bg-gray-800"
+                >
+                    Upload PDF
+                </button>
+
+            </div>
         </div>
     );
 };
