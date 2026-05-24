012a831d79e276548623353440ad66210c1df535
KANAND6031 <krishnaanand2984@gmail.com>
2026-05-24 13:25:57 +0000

    fixed api error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL:
+        "https://friendly-fortnight-4j69wqgx7ggphqqp4-5000.app.github.dev/api",
 });
 
 export default API;
